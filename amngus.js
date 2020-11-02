cf4071ceff39c6a3138bec095f00a3ab7844a503
allinonex6 <63269120+allinonex6@users.noreply.github.com>
2020-11-02 14:36:22 +0100

    Create amngus.js

---
+++ amngus.js
@@ -0,0 +1,149 @@
+[
+{
+"wallpaper_index":"1",
+"wallpaper_name":"Among-Us Wallpapers",
+"wallpaper_site_name":"Among-Us app",
+"wallpaper_site_url":"https://i.pinimg.com/564x/0d/84/93/0d8493680ae2c2ef843cfcd60ee5b026.jpg",
+"wallpaper_url":"https://i.pinimg.com/564x/0d/84/93/0d8493680ae2c2ef843cfcd60ee5b026.jpg"
+},
+{
+"wallpaper_index":"2",
+"wallpaper_name":"Among-Us Wallpapers",
+"wallpaper_site_name":"Among-Us app",
+"wallpaper_site_url":"https://i.pinimg.com/564x/e0/28/d8/e028d8b746d4ae0ec22dcdf7e7a1c508.jpg",
+"wallpaper_url":"https://i.pinimg.com/564x/e0/28/d8/e028d8b746d4ae0ec22dcdf7e7a1c508.jpg"
+},
+{
+"wallpaper_index":"3",
+"wallpaper_name":"Among-Us Wallpapers",
+"wallpaper_site_name":"Among-Us app",
+"wallpaper_site_url":"https://i.pinimg.com/564x/eb/4d/e9/eb4de94089cc53d070bf2f203a5c9c0c.jpg",
+"wallpaper_url":"https://i.pinimg.com/564x/eb/4d/e9/eb4de94089cc53d070bf2f203a5c9c0c.jpg"
+},
+{
+"wallpaper_index":"4",
+"wallpaper_name":"Among-Us Wallpapers",
+"wallpaper_site_name":"Among-Us app",
+"wallpaper_site_url":"https://i.pinimg.com/564x/b8/05/b1/b805b1385ff241b98466d7f11f2ffbc6.jpg",
+"wallpaper_url":"https://i.pinimg.com/564x/b8/05/b1/b805b1385ff241b98466d7f11f2ffbc6.jpg"
+},
+{
+"wallpaper_index":"5",
+"wallpaper_name":"Among-Us Wallpapers",
+"wallpaper_site_name":"Among-Us app",
+"wallpaper_site_url":"https://i.pinimg.com/564x/79/23/ed/7923ed4d40b5a199b8532c4ddd0c647d.jpg",
+"wallpaper_url":"https://i.pinimg.com/564x/79/23/ed/7923ed4d40b5a199b8532c4ddd0c647d.jpg"
+},
+{
+"wallpaper_index":"6",
+"wallpaper_name":"Among-Us Wallpapers",
+"wallpaper_site_name":"Among-Us app",
+"wallpaper_site_url":"https://i.pinimg.com/564x/8c/b8/3d/8cb83d20bad516bcab60b32b92bd1c77.jpg",
+"wallpaper_url":"https://i.pinimg.com/564x/8c/b8/3d/8cb83d20bad516bcab60b32b92bd1c77.jpg"
+},
+{
+"wallpaper_index":"7",
+"wallpaper_name":"Among-Us Wallpapers",
+"wallpaper_site_name":"Among-Us app",
+"wallpaper_site_url":"https://i.pinimg.com/564x/27/b6/b6/27b6b6e535db31213787e65a69e0b68d.jpg",
+"wallpaper_url":"https://i.pinimg.com/564x/27/b6/b6/27b6b6e535db31213787e65a69e0b68d.jpg"
+},
+{
+"wallpaper_index":"8",
+"wallpaper_name":"Among-Us Wallpapers",
+"wallpaper_site_name":"Among-Us app",
+"wallpaper_site_url":"https://i.pinimg.com/564x/a1/a3/37/a1a337b04b8e71db150b7843ec46ef9b.jpg",
+"wallpaper_url":"https://i.pinimg.com/564x/a1/a3/37/a1a337b04b8e71db150b7843ec46ef9b.jpg"
+},
+{
+"wallpaper_index":"9",
+"wallpaper_name":"Among-Us Wallpapers",
+"wallpaper_site_name":"Among-Us app",
+"wallpaper_site_url":"https://i.pinimg.com/564x/01/fd/33/01fd333637c42838129700ca1033c745.jpg",
+"wallpaper_url":"https://i.pinimg.com/564x/01/fd/33/01fd333637c42838129700ca1033c745.jpg"
+},
+{
+"wallpaper_index":"10",
+"wallpaper_name":"Among-Us Wallpapers",
+"wallpaper_site_name":"Among-Us app",
+"wallpaper_site_url":"https://i.pinimg.com/564x/aa/14/30/aa14308d03c93c0979b3380e4bbb017b.jpg",
+"wallpaper_url":"https://i.pinimg.com/564x/aa/14/30/aa14308d03c93c0979b3380e4bbb017b.jpg"
+},
+{
+"wallpaper_index":"11",
+"wallpaper_name":"Among-Us Wallpapers",
+"wallpaper_site_name":"Among-Us app",
+"wallpaper_site_url":"https://i.pinimg.com/564x/7b/7b/81/7b7b818b00279da77524553bd2c09578.jpg",
+"wallpaper_url":"https://i.pinimg.com/564x/7b/7b/81/7b7b818b00279da77524553bd2c09578.jpg"
+},
+{
+"wallpaper_index":"12",
+"wallpaper_name":"Among-Us Wallpapers",
+"wallpaper_site_name":"Among-Us app",
+"wallpaper_site_url":"https://i.pinimg.com/564x/74/f6/51/74f651ce80c4f055fb932edf1b1d1bdc.jpg",
+"wallpaper_url":"https://i.pinimg.com/564x/74/f6/51/74f651ce80c4f055fb932edf1b1d1bdc.jpg"
+},
+{
+"wallpaper_index":"13",
+"wallpaper_name":"Among-Us Wallpapers",
+"wallpaper_site_name":"Among-Us app",
+"wallpaper_site_url":"https://i.pinimg.com/564x/fb/4e/df/fb4edf144ed6fed6e24517eb6d8acbe8.jpg",
+"wallpaper_url":"https://i.pinimg.com/564x/fb/4e/df/fb4edf144ed6fed6e24517eb6d8acbe8.jpg"
+},
+{
+"wallpaper_index":"14",
+"wallpaper_name":"Among-Us Wallpapers",
+"wallpaper_site_name":"Among-Us app",
+"wallpaper_site_url":"https://i.pinimg.com/564x/c1/2d/a2/c12da287e0fcd27cbe4f5905e7754194.jpg",
+"wallpaper_url":"https://i.pinimg.com/564x/c1/2d/a2/c12da287e0fcd27cbe4f5905e7754194.jpg"
+},
+{
+"wallpaper_index":"15",
+"wallpaper_name":"Among-Us Wallpapers",
+"wallpaper_site_name":"Among-Us app",
+"wallpaper_site_url":"https://i.pinimg.com/564x/ec/c7/3a/ecc73a5db63091972ee2009741033679.jpg",
+"wallpaper_url":"https://i.pinimg.com/564x/ec/c7/3a/ecc73a5db63091972ee2009741033679.jpg"
+},
+{
+"wallpaper_index":"16",
+"wallpaper_name":"Among-Us Wallpapers",
+"wallpaper_site_name":"Among-Us app",
+"wallpaper_site_url":"https://i.pinimg.com/564x/be/f2/96/bef29645277dd4acad02259adb5e43bf.jpg",
+"wallpaper_url":"https://i.pinimg.com/564x/be/f2/96/bef29645277dd4acad02259adb5e43bf.jpg"
+},
+{
+"wallpaper_index":"17",
+"wallpaper_name":"Among-Us Wallpapers",
+"wallpaper_site_name":"Among-Us app",
+"wallpaper_site_url":"https://i.pinimg.com/750x/75/4c/dc/754cdcffa18053f1af2dce38f8fbbc05.jpg",
+"wallpaper_url":"https://i.pinimg.com/750x/75/4c/dc/754cdcffa18053f1af2dce38f8fbbc05.jpg"
+},
+{
+"wallpaper_index":"18",
+"wallpaper_name":"Among-Us Wallpapers",
+"wallpaper_site_name":"Among-Us app",
+"wallpaper_site_url":"https://i.pinimg.com/564x/84/49/5d/84495ded1a1276dcec8104b0823a9cfe.jpg",
+"wallpaper_url":"https://i.pinimg.com/564x/84/49/5d/84495ded1a1276dcec8104b0823a9cfe.jpg"
+},
+{
+"wallpaper_index":"19",
+"wallpaper_name":"Among-Us Wallpapers",
+"wallpaper_site_name":"Among-Us app",
+"wallpaper_site_url":"https://i.pinimg.com/564x/b0/0e/6b/b00e6bfd93d8b2ff551baf60bde6b2d2.jpg",
+"wallpaper_url":"https://i.pinimg.com/564x/b0/0e/6b/b00e6bfd93d8b2ff551baf60bde6b2d2.jpg"
+},
+{
+    "wallpaper_index":"20",
+    "wallpaper_name":"Among-Us Wallpapers",
+    "wallpaper_site_name":"Among-Us app",
+    "wallpaper_site_url":"https://i.pinimg.com/564x/71/fa/53/71fa53c922d3a45f47d2888a09fa3a01.jpg",
+    "wallpaper_url":"https://i.pinimg.com/564x/71/fa/53/71fa53c922d3a45f47d2888a09fa3a01.jpg"
+},
+{
+        "wallpaper_index":"21",
+        "wallpaper_name":"Among-Us Wallpapers",
+        "wallpaper_site_name":"Among-Us app",
+        "wallpaper_site_url":"https://i.pinimg.com/564x/4d/c0/f0/4dc0f056e1e6a9773b8327beda987a87.jpg",
+        "wallpaper_url":"https://i.pinimg.com/564x/4d/c0/f0/4dc0f056e1e6a9773b8327beda987a87.jpg"
+}
+] 
